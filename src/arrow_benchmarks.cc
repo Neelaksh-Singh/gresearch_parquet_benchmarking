c49809dfbad47aef90d6e069b6374f31a25a7a01
Neelaksh-Singh <neelaksh48@gmail.com>
2024-06-28 09:43:10 +0530

    Cmake and vcpkg initial setup

---
+++ src/arrow_benchmarks.cc
@@ -0,0 +1,15 @@
+#include <arrow/api.h>
+#include <arrow/io/api.h>
+#include <parquet/arrow/reader.h>
+#include <parquet/arrow/writer.h>
+#include <parquet/parquet_version.h>
+#include <iostream>
+
+int main() {
+    std::cout << "Arrow version: " << ARROW_VERSION_STRING << std::endl;
+    std::cout << "Parquet version: " 
+              << PARQUET_VERSION_MAJOR << "."
+              << PARQUET_VERSION_MINOR << "."
+              << PARQUET_VERSION_PATCH << std::endl;
+    return 0;
+}
