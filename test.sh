9ad3e7d4923ac58c40f8cb86b058e5fde29c399d
Neelaksh-Singh <neelaksh48@gmail.com>
2024-07-18 23:56:40 +0530

    Add script to generate notebook to python and run as a test

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# You may need these if not already installed
+# sudo apt install python3.10-venv
+# sudo apt install jupyter-core
+# sudo apt install jupyter-nbconvert
+
+python3 -m venv ./venv
+source ./venv/bin/activate
+pip install -r requirements.txt
+
+jupyter nbconvert --to python 'Parquet-CPP Benchmarking.ipynb'
+
+# Run the notebook, check for errors
+rm temp/*
+python 'Parquet-CPP Benchmarking.py'
